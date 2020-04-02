c3850cd2ff052de93a5ba34dd89032534c241487
danielJing <danieljing1993@gmail.com>
2020-04-02 19:31:50 +1100

    search bar

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -3,12 +3,14 @@ import React from 'react';
 import TopNav from './TopNav/TopNav'
 import logo from '../assets/logo.png';
 import styles from './LandingPage.module.css';
+import SearchBar from '../SearchBar/SearchBar';
 
 function LandingPage() {
     return (
         <div>
             <TopNav/>
             <img src={logo} className={styles.logo} alt="logo"/>
+            <SearchBar/>
         </div>
     )
 }
---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './SearchBar.module.css';
+
+function SearchBar() {
+    return (
+        <div>
+            <div className="field has-addons">
+                <p className="control">
+                    <button className="button is-static is-medium">Search</button>
+                </p>
+                <p className="control">
+                    <input className={`input is-medium ${styles[`input-control`]}`} type="text" placeholder="burgures,babers,spsa,resturants" />
+                </p>
+                <p className="control">
+                    <button className="button is-static is-medium">Near</button>
+                </p>
+                <p className="control">
+                    <input className={`input is-medium ${styles[`input-control`]}`} type="text" placeholder="Where" />
+                </p>
+
+                <button className={`button is-medium ${styles[`search-button`]}`}>
+                    <span className="icon is-small">
+                        <i className="fas fa-search"></i>
+                    </span>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar
