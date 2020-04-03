adf3f529c27ea106e3807514b63cfda4acd9f16b
danielJing <danieljing1993@gmail.com>
2020-04-03 14:07:02 +1100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import  LandingPage  from './LandingPage/LandingPage';
-
-
+import LandingPage from './LandingPage/LandingPage';
+import Search from './Search/Search';
+import { Route, Switch } from 'react-router-dom'
 function App() {
   return (
-    <LandingPage/>
+    <Switch>
+      <Route path='/search' component={Search} />
+      <Route path='/' component={LandingPage} />
+    </Switch>
   );
 }
 
